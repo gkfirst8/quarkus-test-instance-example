0da883f0f1ce63fc5c4ffb7b92f5c4c8d57b2d06
Gerke Kok - INDI <gerke.kok@indi.nl>
2021-09-15 15:57:18 +0200

    Add @DefaultBean as suggested bu Quarkus support

--- src/main/java/org/thuis99/instancetest/validators/TypeBValidator.java
+++ src/main/java/org/thuis99/instancetest/validators/TypeBValidator.java
@@ -5,6 +5,9 @@ import javax.enterprise.context.ApplicationScoped;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.quarkus.arc.DefaultBean;
+
+@DefaultBean
 @ApplicationScoped
 public class TypeBValidator implements Validator {
     private static final Logger LOG = LoggerFactory.getLogger(TypeBValidator.class);
--- src/test/java/org/thuis99/instancetest/validators/TypeBMockValidator.java
+++ src/test/java/org/thuis99/instancetest/validators/TypeBMockValidator.java
@@ -1,14 +1,11 @@
 package org.thuis99.instancetest.validators;
 
-import javax.enterprise.context.ApplicationScoped;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 // Uncomment these to see the 'Old approach' (See https://quarkus.io/blog/mocking/)
 // import io.quarkus.test.Mock;
 // @Mock
-@ApplicationScoped
 public class TypeBMockValidator extends TypeBValidator {
     private static final Logger LOG = LoggerFactory.getLogger(TypeBMockValidator.class);
 
